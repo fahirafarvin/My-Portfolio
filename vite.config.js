0f5920e9b01f514648de333b8ca7a0128b52b934
fahirafarvin <fahirafarvin04@gmail.com>
2026-06-08 10:49:06 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/My-Portfolio/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: "/",
 });
